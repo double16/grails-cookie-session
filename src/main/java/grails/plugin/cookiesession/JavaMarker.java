549922a188d1d2b094bd60788a51f3f3dc4f6737
Patrick Double <pat@patdouble.com>
2018-01-16 17:13:56 -0600

    Change JavaMarker to interface

--- src/main/java/grails/plugin/cookiesession/JavaMarker.java
+++ src/main/java/grails/plugin/cookiesession/JavaMarker.java
@@ -21,5 +21,5 @@ package grails.plugin.cookiesession;
 /**
  * Multiple Gradle plugins expect a Java output directory. This class will give it to them.
  */
-public class JavaMarker {
+public interface JavaMarker {
 }
